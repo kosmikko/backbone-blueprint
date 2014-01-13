412b219bbe20c19e08803b9649c41e98d11cfbc4
Mikko Lehtinen <mikko@Mikkos-Appl-MacBook-Pro.local>
2014-01-13 15:34:47 +0200

    Default value can be set with a function

--- test/schema.test.js
+++ test/schema.test.js
@@ -1,8 +1,33 @@
 require('chai').should();
+var BaseModel = require('..').Model;
 var Schema = require('..').Schema;
 var addressSchema = require('./fixtures').addressSchema;
 
 describe('Test schema', function () {
+
+  it('should set default attributes with a function', function() {
+    var testSchema = {
+      id: '/schemas/test',
+      type: 'object',
+      properties: {
+        id: {
+          type: 'integer'
+        },
+        date: {
+          type: 'date',
+          default: function() {
+            return new Date();
+          }
+        }
+      }
+    };
+    var TestModel = BaseModel.extend({
+      type: 'test',
+      schema: testSchema
+    });
+    var t = new TestModel();
+    t.get('date').should.be.an.instanceof(Date);
+  });
 
   it('should extend schema', function() {
     var newSchema = {
